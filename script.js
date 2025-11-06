74875022ab8836251810c9ffc7c5963720295ce1
Bobo <fansditz@gmail.com>
2025-11-06 20:48:31 +0800

    基本架構

---
+++ script.js
@@ -0,0 +1,24 @@
+const songs = ["distance.mp3"];
+let current = 0;
+
+const player = document.getElementById("player");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+
+playBtn.addEventListener("click", () => {
+  if (player.paused) player.play();
+  else player.pause();
+});
+
+nextBtn.addEventListener("click", () => {
+  current = (current + 1) % songs.length;
+  player.src = songs[current];
+  player.play();
+});
+
+prevBtn.addEventListener("click", () => {
+  current = (current - 1 + songs.length) % songs.length;
+  player.src = songs[current];
+  player.play();
+});
