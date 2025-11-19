9cfa053113233241aa2ddc8bc01c3bdf1c7ea724
Bobo <fansditz@gmail.com>
2025-11-19 12:59:29 +0800

    [新增]隨機/重複撥放、歌名/歌手[刪除]播放/暫停

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
-const songs = ["music/distance.mp3", "music/song.mp3"];
-let current = 0;
+const songs = [
+  {
+    src: "music/distance.mp3",
+    name: "distance",
+    artist: "bixby"
+  },
+  {
+    src: "music/song.mp3",
+    name: "沒出息",
+    artist: "王世堅"
+  },
+  {
+    src: "music/intentions.mp3",
+    name: "intentions",
+    artist: "starfall"
+  }
+];
 
+let current = 0;
 const player = document.getElementById("player");
-const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
+const shuffleBtn = document.getElementById("shuffle");
+const repeatBtn = document.getElementById("repeat");
 
-playBtn.addEventListener("click", () => {
-  if (player.paused) {
-    player.play().catch(e => console.log("播放被阻擋", e));
-  } else {
-    player.pause();
-  }
-});
+function updateInfo() {
+  document.getElementById("song-title").textContent = 
+  `${songs[current].name} - ${songs[current].artist}`;
+}
 
 
+function loadSong() {
+  player.src = songs[current].src;
+  updateInfo();
+}
+
+// 下一首
 nextBtn.addEventListener("click", () => {
   current = (current + 1) % songs.length;
-  player.src = songs[current];
-  player.play().catch(e => console.log("播放被阻擋", e));
-});
-
-
-prevBtn.addEventListener("click", () => {
-  current = (current - 1 + songs.length) % songs.length;
-  player.src = songs[current];
+  loadSong();
   player.play();
 });
+
+// 上一首
+prevBtn.addEventListener("click", () => {
+  current = (current - 1 + songs.length) % songs.length;
+  loadSong();
+  player.play();
+});
+
+shuffleBtn.addEventListener("click", () => {
+  current = Math.floor(Math.random() * songs.length);
+  loadSong();
+  player.play();
+});
+
+repeatBtn.addEventListener("click", () => {
+  player.loop = !player.loop;
+  repeatBtn.textContent = player.loop ? "取消重複" : "重複播放";
+});
+
+
+// 首次初始化
+loadSong();
