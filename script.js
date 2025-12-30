30592dbea1d0c265ece157254fe038a4bd528b51
Bobo <fansditz@gmail.com>
2025-12-30 21:39:07 +0800

    【新增】歌曲-左轉燈、太陽與地球

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ const shuffleBtn = document.getElementById('shuffle');
 // 根據你的截圖，檔名應該是這些：
 const songs = [
     { title: "沒出息", src: "music/song.mp3", artist:"王世堅" },
-    { title: "Distance", src: "music/distance.mp3",artist:"bixby" },
-    { title: "Intentions", src: "music/intentions.mp3", artist:"starfall"}
+    { title: "Distance", src: "music/distance.mp3", artist:"bixby" },
+    { title: "Intentions", src: "music/intentions.mp3", artist:"starfall"},
+    { title: "太陽與地球", src: "music/sun_and_earth.m4a", artist:"盧廣仲" },
+    { title: "左轉燈", src: "music/turn_left_light.m4a", artist:"超派" }
 ];
 
 let currentindex = 0;
